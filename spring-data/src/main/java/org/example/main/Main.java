870e7d7c3502f5af86940bc88bb1a4c46c237ebb
vee-loves-code <viviansteph172@gmail.com>
2023-03-28 03:41:16 +0100

    edits adding more instances of EntityManager

--- spring-data/src/main/java/org/example/main/Main.java
+++ spring-data/src/main/java/org/example/main/Main.java
@@ -14,13 +14,28 @@ public class Main {
        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA Basics");
        EntityManager em = emf.createEntityManager();
        Product p = new Product();
-       p.setId(2);
-       p.setName("wine");
-       p.setPrice(200 );
+       p.setId(5);
+       p.setName("Champagne");
+       p.setPrice(202 );
        p.setExpiration_date(LocalDate.now());
         em.getTransaction().begin();
         em.persist(p);
         em.getTransaction().commit();
         em.close();
+
+        Product p1= new Product();
+        p1.setId(6);
+        p1.setName("Hot");
+        p1.setPrice(104);
+        p1.setExpiration_date(LocalDate.now());
+
+        var em1 = emf.createEntityManager();
+
+        em1.getTransaction().begin();
+
+        em1.persist(p1); // adding the instance in the context
+
+        em1.getTransaction().commit();
+        em1.close();
     }
 }
