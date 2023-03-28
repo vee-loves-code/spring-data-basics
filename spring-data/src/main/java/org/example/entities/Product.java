3a2583f88bb0e96f3cb04c178a49daf8fccfba17
vee-loves-code <viviansteph172@gmail.com>
2023-03-28 03:32:12 +0100

    first pesrist EntityManagerFactory

---
+++ spring-data/src/main/java/org/example/entities/Product.java
@@ -0,0 +1,49 @@
+package org.example.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Product {
+    @Id
+    private int id;
+    private String name;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getExpiration_date() {
+        return expiration_date;
+    }
+
+    public void setExpiration_date(LocalDate expiration_date) {
+        this.expiration_date = expiration_date;
+    }
+
+    @Column(name = "expiration_date")
+    private LocalDate expiration_date;
+}
