96b474bce7d63a2b37e79e6407a254a933b4ce23
vee-loves-code <viviansteph172@gmail.com>
2023-04-03 23:05:37 +0100

    still using entitymanager, trying out the id generation strategy using table GenerationType and customizing column properties

--- spring-data/src/main/java/org/example/entities/Product.java
+++ spring-data/src/main/java/org/example/entities/Product.java
@@ -6,14 +6,19 @@ import java.time.LocalDate;
 @Entity
 public class Product {
     @Id
+    @TableGenerator(name = "key_generator", table = "key_generator",
+        pkColumnName = "key_name",
+        pkColumnValue = "product_value",
+        valueColumnName = "key_value")
     @GeneratedValue(strategy = GenerationType.TABLE, generator = "key_generator")
     private Integer id;
     private String name;
 
-    //here another table called "key_generator" is created for the id genetion. two columns are created
-    // one called sequence_name, set as PK and NN, Varchar type
-    //the other called next_val : this store the range in which the value of the next id will be
-    // INT type
+    //GenerationType.TABLE
+    //here another table called "key_generator" is created for the id genetion. two columns are created, these two column's names are customizable. using the table method.
+    // sequence_name is an hibernate specified name, it can be called anything else. e.g key_name, set as PK and NN, Varchar type
+    //next_val is also an hibernate specified name, it can be called anything else. e.g key_val: this stores the range in which the value of the next id will be, INT type
+    // in orger to customize, we must inform hibernate using the @TableGenerator annotation
 
     public int getId() {
         return id;
